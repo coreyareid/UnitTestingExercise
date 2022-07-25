6d9c28c8e39907612ac4ce8f6eea8bf5769bb22f
coreyareid <coreyareid@bellsouth.net>
2022-07-25 14:17:26 -0500

    Added 3 InlineData per method

--- UnitTestingExercise.Tests/UnitTests.cs
+++ UnitTestingExercise.Tests/UnitTests.cs
@@ -25,7 +25,9 @@ namespace UnitTestingExercise.Tests
         }
 
         [Theory]
-        [InlineData(6, 5, 1)]//Add test data <-------
+        [InlineData(6, 5, 1)]
+        [InlineData(10, 4, 6)]
+        [InlineData(90, 120, -30)]//Add test data <-------
         public void SubtractTest(int minuend, int subtrhend, int expected)
         {
             //Start Step 5 here:
@@ -41,7 +43,9 @@ namespace UnitTestingExercise.Tests
         }
 
         [Theory]
-        [InlineData(3, 5, 15)]//Add test data <-------
+        [InlineData(3, 5, 15)]
+        [InlineData(10, 10, 100)]
+        [InlineData(2, 35, 70)]//Add test data <-------
         public void MultiplyTest(int num1, int num2, int expected)
         {
             //Start Step 7 here:
@@ -57,7 +61,9 @@ namespace UnitTestingExercise.Tests
         }
 
         [Theory]
-        [InlineData(6, 3, 2)]//Add test data <-------
+        [InlineData(6, 3, 2)]
+        [InlineData(9, 3, 3)]
+        [InlineData(21, 7, 3)]//Add test data <-------
         public void DivideTest(int num1, int num2, int expected)
         {
             //Arrange
